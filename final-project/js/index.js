d847688759921e73dfa0c3bfe9c527bb3f603d3e
gnzlplcs <palaciosgm@outlook.com>
2021-07-14 23:57:57 -0500

    connection and displaying done. it lacks to implement MVC.

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -1,3 +1,5 @@
+import WordView from "./view.js";
+
 const myToken = 'ff0ae246-89ef-493b-ae2a-abf4bc45581f';
 const API = `https://dictionaryapi.com/api/v3/references/collegiate/json/`;
 const searchBtn = document.getElementById('search-btn');
@@ -9,19 +11,41 @@ const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 // event listeners
 searchBtn.addEventListener('click', getWordList);
-// // wordList.addEventListener('click', getWordRecipe);
-// // recipeCloseBtn.addEventListener('click', () => {
-// //   wordDetailsContent.parentElement.classList.remove('showRecipe');
+// // wordList.addEventListener('click', getWordDefinition);
+// // definitionCloseBtn.addEventListener('click', () => {
+// //   wordDetailsContent.parentElement.classList.remove('showDefinition');
 // // })
 
 // get word list that matches with the search
 function getWordList() {
   let searchInputTxt = document.getElementById('search-input').value.trim();
   let url_api = `${API}${searchInputTxt}?key=${myToken}`;
+  // let word = new WordView();
   fetch(url_api)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => renderWord(data))
 }
+
+function renderWord(word) {
+  let wordItem = document.createElement('div');
+  let wordName = document.createElement('div');
+  wordItem.classList.add('word-item');
+  wordName.classList.add('word-name');
+  wordName.innerHTML = `
+    <h3>${word[0].meta.id}</h3>
+    <p>${word[0].fl}</p>
+    <p>${word[0].shortdef}</p>
+    `;
+  wordItem.appendChild(wordName);
+  console.log(wordItem)
+  wordList.appendChild(wordItem);
+  return wordItem;
+}
+
+
+// data[0].meta.id => get the id word (title)
+// data[0].fl => get the functional label word (subtitle)
+// data[0].shortdef => returns an array of short definitions
 
 
 // function getWordList() {
@@ -34,9 +58,6 @@ function getWordList() {
 //       // data.meals.forEach(meal => {
 //       //   html += `
 //       //   <div class="meal-item" data-id="${meal.idMeal}">
-//       //     <div class="meal-img">
-//       //       <img src="${meal.strMealThumb}" alt="food image">
-//       //     </div>
 //       //     <div class="meal-name">
 //       //       <h3>${meal.strMeal}</h3>
 //       //       <a href="#" class="recipe-btn">Get Recipe</a>
