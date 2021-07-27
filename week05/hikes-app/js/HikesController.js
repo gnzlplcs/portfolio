454e0bb6a4df82a3c76714fa2ab8a3ba19853c1b
gnzlplcs <palaciosgm@outlook.com>
2021-07-27 10:48:55 -0500

    Now, it works

--- week05/hikes-app/js/HikesController.js
+++ week05/hikes-app/js/HikesController.js
@@ -1,40 +1,38 @@
-// The controller needs access to both the model and the view...so let's import them
-import HikeModel from './hikeModel.js';
-import HikesView from './hikesView.js';
+import HikeModel from "./HikeModel.js";
+import HikesView from "./HikesView.js";
 
-// Just like with the view we should organize the functions we need to our controller. Let's use a class for this one
-
+// Hike controller
 export default class HikesController {
-  // a class needs a constructor
   constructor(parentId) {
     this.parentElement = document.getElementById(parentId);
+    // this is how our controller will know about the model and view...we add them right into the class as members.
     this.hikeModel = new HikeModel();
     this.hikesView = new HikesView(parentId);
   }
+
   showHikeList() {
-    // the list of hikes will come from the model now...
+    //  this will get called each time we need to display our full hike list. It should grab the list of hikes from the Model, and then send them to the view.
     const hikeList = this.hikeModel.getAllHikes();
-    // send the list of hikes and the element we would like those placed into to the view.
     this.hikesView.renderHikeList(this.parentElement, hikeList);
-    // after the hikes have been rendered...add our listener
     this.addHikeListener();
   }
+
   showOneHike(hikeName) {
+    // use this when you need to show just one hike...with full details
     const hike = this.hikeModel.getHikeByName(hikeName);
-    this.hikesView.renderOneHikeFull(
-      this.parentElement,
-      hike
-    ).ontouchend = () => {
+    const myTouchend = this.hikesView.renderOneHikeFull(this.parentElement, hike);
+    myTouchend.ontouchend = () => {
       this.showHikeList();
     };
   }
-  // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
+
   addHikeListener() {
+    // for the stretch you will need to attach a listener to each of the listed hikes to watch for a touchend.
+
     // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
     const childrenArray = Array.from(this.parentElement.children);
     childrenArray.forEach(child => {
       child.addEventListener('touchend', e => {
-        // why currentTarget instead of target?
         this.showOneHike(e.currentTarget.dataset.name);
       });
     });
---
+++ week05/hikes-app/js/main.js
@@ -0,0 +1,4 @@
+import HikesController from './HikesController.js';
+
+const myHike = new HikesController('hikesList');
+myHike.showHikeList();
